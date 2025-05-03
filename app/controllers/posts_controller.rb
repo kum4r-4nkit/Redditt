16a10fb50970c57cc5fe3cc5f353d7bb35c6f394
ankitK <31028413+kum4r-4nkit@users.noreply.github.com>
2025-05-03 19:31:27 +0530

    Add pagination to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,10 +5,27 @@ class PostsController < ApplicationController
   before_action :authorize_request, except: [:index, :show]  # Or remove `except:` to protect all
   before_action :set_post, only: [:show, :update, :destroy]
 
-
   def index
-    posts = Post.includes(:user, :comments).all
-    render json: posts.as_json(include: [:user, :comments])
+    page = params[:page].to_i > 0 ? params[:page].to_i : 1
+    per_page = params[:per_page].to_i > 0 ? params[:per_page].to_i : 10
+  
+    total_count = Post.count
+    total_pages = (total_count.to_f / per_page).ceil
+  
+    posts = Post.includes(:user, comments: :user)
+                .order(created_at: :desc)
+                .offset((page - 1) * per_page)
+                .limit(per_page)
+  
+    render json: {
+      posts: ActiveModelSerializers::SerializableResource.new(posts),
+      meta: {
+        current_page: page,
+        per_page: per_page,
+        total_count: total_count,
+        total_pages: total_pages
+      }
+    }
   end
 
   def show
---
+++ app/serializers/post_serializer.rb
@@ -0,0 +1,6 @@
+class PostSerializer < ActiveModel::Serializer
+  attributes :id, :title, :body, :created_at
+
+  belongs_to :user
+  has_many :comments
+end
