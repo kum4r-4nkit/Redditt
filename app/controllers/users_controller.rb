86be726386c1b56d23cd94827ee1fe73c4e9c4f7
ankitK <31028413+kum4r-4nkit@users.noreply.github.com>
2025-05-08 12:00:27 +0530

    Update password in UsersController

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,40 @@
+# app/controllers/users_controller.rb
+class UsersController < ApplicationController
+  include AuthorizeRequest
+
+  before_action :authorize_request
+
+  def show
+    render json: @current_user
+  end
+
+  def update
+    if @current_user.update(user_params)
+      render json: @current_user
+    else
+      render json: { errors: @current_user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update_password
+    if @current_user.authenticate(params[:current_password])
+      if @current_user.update(password_params)
+        render json: { message: 'Password updated successfully' }
+      else
+        render json: { errors: @current_user.errors.full_messages }, status: :unprocessable_entity
+      end
+    else
+      render json: { error: 'Current password is incorrect' }, status: :unauthorized
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :bio)
+  end
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+end
