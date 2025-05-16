f71f56da94ba3c27be86a640dbd81049d0b9dfc9
ankitK <31028413+kum4r-4nkit@users.noreply.github.com>
2025-05-16 12:13:59 +0530

    Edit API response fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,7 @@
 # app/controllers/posts_controller.rb
 class PostsController < ApplicationController
   include AuthorizeRequest
-
   before_action :authorize_request, except: [:index, :show]  # Or remove `except:` to protect all
-  before_action :set_post, only: [:update, :destroy]
 
   def index
     page = params[:page].to_i > 0 ? params[:page].to_i : 1
@@ -43,34 +41,32 @@ class PostsController < ApplicationController
   end
 
   def update
-    if @post.user_id != @current_user.id
+    post = Post.includes(:user, comments: :user).find(params[:id])
+    if post.user_id != @current_user.id
       render json: { error: "Not authorized" }, status: :unauthorized
       return
     end
   
-    if @post.update(post_params)
-      render json: @post
+    if post.update(post_params)
+      render json: post, include: ['user', 'comments.user']
     else
-      render json: { errors: @post.errors.full_messages }, status: :unprocessable_entity
+      render json: { errors: post.errors.full_messages }, status: :unprocessable_entity
     end
   end
   
 
   def destroy
-    if @post.user_id != @current_user.id
+    post = Post.find(params.expect(:id))
+    if post.user_id != @current_user.id
       render json: { error: "Not authorized" }, status: :unauthorized
       return
     end
 
-    @post.destroy!
+    post.destroy!
     head :no_content
   end
 
   private
-
-  def set_post
-    @post = Post.find(params.expect(:id))
-  end
 
   def post_params
     params.require(:post).permit(:title, :body)
