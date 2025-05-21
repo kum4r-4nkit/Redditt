95deae6769e76809812c4435ce2f8796abc4d392
ankitK <31028413+kum4r-4nkit@users.noreply.github.com>
2025-05-21 15:12:59 +0530

    CORS error fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'http://localhost:5173', 'https://reddditt.netlify.app'
     resource '*',
       headers: :any,
       methods: [:get, :post, :patch, :put, :delete, :options, :head],
