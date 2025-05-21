bddcf6ded5e99df5010afaecd552896b53a1c9a1
ankitK <31028413+kum4r-4nkit@users.noreply.github.com>
2025-05-21 14:53:06 +0530

    Trying render deployment

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173' # React dev server
-
+    origins '*'
     resource '*',
       headers: :any,
       methods: [:get, :post, :patch, :put, :delete, :options, :head],
